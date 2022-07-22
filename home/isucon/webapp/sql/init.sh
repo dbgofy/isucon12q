e27a1dbbedf4ab3679e3093e3d05f4f8693bef9b
Atsushi Tanaka <bgpat23@gmail.com>
2022-07-23 06:02:37 +0900

    1.db

--- home/isucon/webapp/sql/init.sh
+++ home/isucon/webapp/sql/init.sh
@@ -20,4 +20,18 @@ mysql -u"$ISUCON_DB_USER" \
 rm -f ../tenant_db/*.db
 cp -r ../../initial_data/*.db ../tenant_db/
 
+cat 1.db.dump | grep 'INSERT INTO player_score' | sed 's/^.*VALUES\|;$/"/g' | xargs -n10000 | tr ' ' ',' | sed 's/^/INSERT INTO player_score VALUES /' | sed 's/$/;/' > insert1_player_score.sql
+cat 1.db.dump | grep -v 'INSERT INTO player_score' > insert1.sql
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < insert1.sql
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < insert1_player_score.sql
+mv ../tenant_db/1.db ./1.db
 find ../tenant_db/ -name '*.db' | parallel --jobs 100 "./sqlite3-to-sql {} | mysql -u$ISUCON_DB_USER -p$ISUCON_DB_PASSWORD --host $ISUCON_DB_HOST --port $ISUCON_DB_PORT $ISUCON_DB_NAME"
+mv ./1.db ../tenant_db/1.db
